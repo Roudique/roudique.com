ebcb17beab0805f8ce3102519eeb89a97cb22882
Roudique <roudique@gmail.com>
2018-01-29 23:30:13 -0500

    Add basic portfolio page

--- Sources/App/Droplet+Setup.swift
+++ Sources/App/Droplet+Setup.swift
@@ -2,7 +2,15 @@
 
 extension Droplet {
     public func setup() throws {
-        let routes = Routes(view)
-        try collection(routes)
+//        let routes = Routes(view)
+//        try collection(routes)
+        
+        self.get("") { request in
+            return try self.view.make("welcome")
+        }
+        
+        self.get("portfolio") { request in
+            return try self.view.make("portfolio")
+        }
     }
 }
